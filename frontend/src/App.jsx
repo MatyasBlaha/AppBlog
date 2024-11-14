5b73ff0f699179ebc28282717a74f8ba8c34fece
MatyasBlaha <matyasblaha1@gmail.com>
2024-11-14 23:12:28 +0100

    fix: delete comma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
                             {
                                 path: ':postId', children: [
                                     {index: true, element: <PostDetailPage/>}
-                                ],
+                                ]
                             }
                         ]
                     }
