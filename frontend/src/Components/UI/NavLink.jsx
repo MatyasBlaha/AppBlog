79f3eb06e44e9323c8ff5ba37cf6269c9c190e70
MatyasBlaha <matyasblaha1@gmail.com>
2024-11-14 22:52:20 +0100

    style: add style to NavLink component

--- frontend/src/Components/UI/NavLink.jsx
+++ frontend/src/Components/UI/NavLink.jsx
@@ -1,8 +1,16 @@
 import { NavLink} from "react-router-dom";
 export function NavigationLink({ to, children}) {
+
     return (
             <li>
-                <NavLink to={to} end>
+                <NavLink
+                    to={to}
+                    className={({isActive}) => isActive
+                        ? 'text-gray-50 no-underline border-b-2'
+                        : 'relative text-gray-300 text-md w-fit block after:block after:content-[\'\'] after:absolute after:h-[2px] after:bg-gray-300 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left'
+                }
+                    end
+                >
                     {children}
                 </NavLink>
             </li>
