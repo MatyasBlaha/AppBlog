08d59a9539ecaaa03d3a914000d3701a0447e0e8
MatyasBlaha <matyasblaha1@gmail.com>
2024-11-14 18:02:53 +0100

    feat: initial route config and RootLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootPage from "./pages/Root.jsx";
 
 function App() {
+  const router = new createBrowserRouter([
+    {
+      path: '/',
+      element: <RootPage />
+    },
+  ])
 
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
   )
 }
