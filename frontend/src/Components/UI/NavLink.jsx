e86ed9c6f9fe196d277a71f82f9dcc2fc9549522
MatyasBlaha <matyasblaha1@gmail.com>
2024-11-17 21:12:11 +0100

    refactor: add ...props and change styles

--- frontend/src/Components/UI/NavLink.jsx
+++ frontend/src/Components/UI/NavLink.jsx
@@ -1,15 +1,16 @@
 import { NavLink} from "react-router-dom";
-export function NavigationLink({ to, children}) {
+export function NavigationLink({ to, children, ...props}) {
 
     return (
-            <li>
+            <li className='list-none'>
                 <NavLink
+                    {...props}
                     to={to}
                     className={({isActive}) => isActive
                         ? 'text-gray-50 no-underline border-b-2'
                         : 'relative text-gray-300 text-md w-fit block after:block after:content-[\'\'] after:absolute after:h-[2px] after:bg-gray-300 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left'
                 }
-                    end
+
                 >
                     {children}
                 </NavLink>
